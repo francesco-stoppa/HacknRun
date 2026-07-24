78ead12438cc19886f7c348ced965d1684987bec
Fra_Archprogrammer <francesco.stoppa99@gmail.com>
2026-07-24 17:31:19 +0200

    Hacking
    La meccanica dell'haching avviene in determinate condizioni:
    1. Il gameObject del Player (e di quello che si vuole hackerare) ha lo script <RobotController> (si anche per la telecamera, ma per settare la telecamera di modo che non si muova c'è la nuova variabile <VideoCamera>).
    2. Il gamObject del Player ha la bool <activeCharacter> true.
    3. Il gameObject del Player (e di quello che si vuole hackerare) ha una camera ha lo script <TargetLock>.
    4. Il gameObject che si vuole hackerare ha il tag "Player".

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -4,20 +4,46 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class RobotController : MonoBehaviour
 {
-    [SerializeField] private Transform cameraTransform;
+    [Tooltip("Place the camera here.")]
+    public Transform cam;
+    private AudioListener a;
+    public bool videoCamera = false;
+    [SerializeField] private bool activeCharacter = false;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float gravity = -9.81f;
 
     private CharacterController controller;
     private float verticalVelocity;
 
+    private Camera c;
+
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        c = cam.gameObject.GetComponent<Camera>();
+
+        if (c == null) return;
+        c.enabled = false;
+        if (cam == null) return;
+        a = cam.gameObject.GetComponent<AudioListener>();
+        a.enabled = false;
+
+        if (activeCharacter)
+            ActiveCharacter();
+    }
+
+    public void ActiveCharacter()
+    {
+        c.enabled = true;
+        activeCharacter = true;
+        if (a == null) return;
+        a.enabled = true;
     }
 
     private void Update()
     {
+        if (activeCharacter == false || videoCamera == true) return;
+
         // Input WASD
         Vector2 input = Keyboard.current == null
             ? Vector2.zero
@@ -29,8 +55,8 @@ public class RobotController : MonoBehaviour
         input = Vector2.ClampMagnitude(input, 1f);
 
         // Direzioni della camera sul piano XZ
-        Vector3 forward = cameraTransform.forward;
-        Vector3 right = cameraTransform.right;
+        Vector3 forward = cam.forward;
+        Vector3 right = cam.right;
 
         forward.y = 0f;
         right.y = 0f;
